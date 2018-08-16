d77a096f134349c3301b54b656e8e0d6c74e708a
SorMV <33803127+SorMV@users.noreply.github.com>
2018-08-16 15:31:01 +0300

    Update MapFromBD.js

--- MapFromBD.js
+++ MapFromBD.js
@@ -20,27 +20,26 @@ function init() {
     
 
     function onZonesLoad(json) {
-        // ��������� �������� �� �����.
+      
         polygon = new ymaps.Polygon(json.coordinates);
         var deliveryZones = ymaps.geoQuery(polygon).addToMap(myMap);
         
-        // �������� ��������� ����� ���������� ������ �������� ��������
+       
         myMap.controls.get('geolocationControl').events.add('locationchange', function (e) {   
             highlightResult(e.get('geoObjects').get(0));
         });
 
         function highlightResult(obj) {
-            // ���� ����������
+            
             var coords = obj.geometry.getCoordinates(),
          
-            // �������. � ������� ������� ���� �����
+            
                 ispolygon = deliveryZones.searchContaining(coords).get(0);
                 deliveryPoint.geometry.setCoordinates(coords);
            //ispolygon=coords.searchInside(polygon);
            
             if (ispolygon) {
-                // ���������� ����� � ���������� ���������� � ������������� �� � ���� ��������.
-              // deliveryPoint.geometry.setCoordinates([30.313462, 59.939017]);
+             
              myMap.geoObjects.add(new ymaps.Placemark([30.313462, 59.939017], {
             balloonContent: '<strong>Point is inside in the Polygon!</strong>'
         }, {
@@ -51,17 +50,15 @@ function init() {
                 if (typeof(obj.getThoroughfare) === 'function') {
                     setData(obj);
                 } else {
-        			//������ ��������� ��� ����������� �����
+        		
                     ymaps.geocode(coords, {results: 1}).then(function (res) {
                         var obj = res.geoObjects.get(0);
                         setData(obj);
                     });
                 }
             } else {
-                // ���� ���������� ���������� �� �������� � �������, �� ������� ������� ��������, ��������, ������, ���� �� �� �������� �� � ���� ������� 
-                deliveryZones.setOptions('fillOpacity', 0.1);
-                // ���������� ����� �� ���������� �����������.
-                deliveryPoint.geometry.setCoordinates(coords);
+              deliveryZones.setOptions('fillOpacity', 0.1);
+              deliveryPoint.geometry.setCoordinates(coords);
             }  
             
         }
